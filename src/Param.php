7cd8238f7fea9297391d1a087ea867343bdee128
付义兵 <websearch@163.com>
2018-03-20 16:39:35 +0800

    debug: 空值验证

--- src/Param.php
+++ src/Param.php
@@ -167,7 +167,7 @@ class Param
             }
 
             // 1.5 空值检查
-            if (empty($data[$key]) && (!isset($rule['empty']) || !$rule['empty'])) {
+            if (empty($data[$key]) && (!isset($rule['empty']) || '' === $rule['empty'])) {
                 throw new ParamException("字段 '{$key}' 不能为空", 10000);
             }
 
