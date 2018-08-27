e57c45fb2c2b4f1de00f06335b478f485d3b7324
fuyibing <websearch@163.com>
2018-08-27 11:01:33 +0800

    Update BooleanValidator.php

--- src/Validators/BooleanValidator.php
+++ src/Validators/BooleanValidator.php
@@ -2,31 +2,15 @@
 /**
  * 框架级Validator
  * @author wsfuyibing <websearch@163.com>
- * @date   2018-01-05
+ * @date   2018-08-27
  */
 namespace Uniondrug\Validation\Validators;
 
 use Phalcon\Validation;
-use Phalcon\Validation\Message;
-use Uniondrug\Validation\Structs\DatetimeParseStruct;
 use Uniondrug\Validation\Validator;
 
 /**
- * 验证带日期的完整时间格式
- * <code>
- * $validation = new Validation();                  // 创建Validation实例
- * $attribute = 'field';                            // 参数名称
- * $options = [                                     // 验证选项
- *     'required' => 'true',                        // 是否必须
- *     'empty' => 'false',                          // 是否允许为空
- *     'default' => '2018-01-15 12:30',             // 当不传字段时赋默认值
- *     'min' => '2018-01-01 08:00',                 // 最小时间
- *     'max' => '2018-12-31 21:30'                  // 最大时间
- * ];
- * $validator = new DatetimeValidator($options);
- * $validation->add($attribute, $validator);
- * $validation->validate();
- * </code>
+ * Boolean类型
  * @package Pails\Validators
  */
 class BooleanValidator extends Validator
